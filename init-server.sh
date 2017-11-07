aa18a578009c95d5555deab588771b6c2f11be04
codingdie <547912355@qq.com>
2017-11-07 20:08:05 +0800

    Update init-server.sh

--- init-server.sh
+++ init-server.sh
@@ -4,7 +4,7 @@ git config --global user.name  $1
 echo ${4//./_}>/etc/hostname
 echo "127.0.0.1 localhost ${4//./_}">/etc/hosts
 echo "${5} ${4}">>/etc/hosts
-chmod 700 /opt
+chmod 755 /opt
 sh install-jdk-8.sh
 sh install-common.sh
 reboot
